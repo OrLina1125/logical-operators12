d723eb6ca083926eac672e6b48ace3cd6501c116
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-23 11:29:54 +0900

    add Logical Operators

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+<?php
+//Logical Operators
+$x = 10;
+$y = 20;
+
+//if ($x==$y ||(sama dengan or)1 == 1){
+//if ($x==$y && (sama dengan and)1 == 1){
+if ($x==$y xor 1 == 1){
+    echo "True!";
+}
+
+?>
+
+</body>
+</html>
+
